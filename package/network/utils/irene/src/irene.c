2a86c4b12ff63625187cf842fd5fd3f22e6aa86f
Manbing <manbing3@gmail.com>
2021-07-17 14:48:32 +0800

    Package: add debug compile option
    Signed-off-by: Manbing <manbing3@gmail.com>

--- package/network/utils/irene/src/irene.c
+++ package/network/utils/irene/src/irene.c
@@ -1,43 +1,43 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
 #include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define PORC_FILE "/proc/kernel_memleak"
 #define TYPE 'j'
-#define CMD_GET                 _IOR(TYPE, 1, int)
-#define CMD_SET                 _IOW(TYPE, 2, int)
+#define CMD_GET _IOR(TYPE, 1, int)
+#define CMD_SET _IOW(TYPE, 2, int)
 
 int main(void)
 {
-        int fd = 0, cmd = 0, ret = 0;
-        
-        fd = open(PORC_FILE, O_RDWR);
+	int fd = 0, cmd = 0, ret = 0;
 
-        assert(fd);
+	fd = open(PORC_FILE, O_RDWR);
 
+	assert(fd);
 
-        while (1) {
-                printf("\n\n\n");
-                printf("function menu\n");
-                printf("1. show\n");
-                printf("\n");
-                printf("please enter command which you want:\n");
-                scanf("%d", &cmd);
+	while (1) {
+		printf("\n\n\n");
+		printf("function menu\n");
+		printf("1. show\n");
+		printf("\n");
+		printf("please enter command which you want:\n");
+		scanf("%d", &cmd);
 
-                switch (cmd) {
-                        case 1:
-                                ret = ioctl(fd, CMD_GET, NULL);
-                                break;
-                        
-                        default:
-                                goto out;
-                }
-        }
+		switch (cmd) {
+		case 1:
+			ret = ioctl(fd, CMD_GET, NULL);
+			break;
+
+		default:
+			goto out;
+		}
+	}
 
 out:
-        close(fd);
-        return 0;
+	close(fd);
+	return 0;
 }
